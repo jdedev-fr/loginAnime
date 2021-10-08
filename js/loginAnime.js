e7101b96a6925a41559d6f42e74d465ece43b0ad
JdeDev <julien@jdedev.fr>
2021-10-08 12:09:08 +0200

    ajout espace

--- js/loginAnime.js
+++ js/loginAnime.js
@@ -4,6 +4,7 @@ let form1 = document.querySelector('.form1')
 let form2 = document.querySelector('.form2')
 let login = document.querySelector('.login')
 let inscr = document.querySelector('.inscr')
+
 btnSlide.addEventListener('click', () => {
     let l = btn.style.left
     btn.style.left = (l == "" || l == "0px") ? '120px' : '0px'
